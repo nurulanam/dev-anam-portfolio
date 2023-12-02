f15feb674bccfa114d584f4b9aabb969f7b52fa0
nurulanam <nurulanam.bd@gmail.com>
2023-12-02 14:51:29 +0600

    Feat: google verification with sitemap.xml

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="0nLit_UstaOqJ-EyyJJcyB0q0wQOCT1AUEjr4i15gl4" />
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/img/favicon-96x96.png') }}">
     <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="{{ asset('/img/favicon-180x180.png') }}">
     @yield('extraMeta')
