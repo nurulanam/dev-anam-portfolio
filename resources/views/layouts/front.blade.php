aaf8af33db23f2d005d0003e48a4046845a3ab3f
nurulanam <nurulanam.bd@gmail.com>
2023-12-06 14:48:47 +0600

    Fix: favicons

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="google-site-verification" content="0nLit_UstaOqJ-EyyJJcyB0q0wQOCT1AUEjr4i15gl4" />
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/img/favicon-96x96.png') }}">
-    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="{{ asset('/img/favicon-180x180.png') }}">
+    <<meta name="google-site-verification" content="0nLit_UstaOqJ-EyyJJcyB0q0wQOCT1AUEjr4i15gl4" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/favicon-96x96.png') }}">
+    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="{{ asset('/favicon-180x180.png') }}">
     @yield('extraMeta')
     {!! SEO::generate() !!}
     @vite('resources/js/app.js')
